cc7185787d60859758cab8713eef44d39c7beb3d
Nikolay Mishin <mishin.nikolay.d3@mail.ru>
2021-08-11 00:04:12 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,8 +5,8 @@ define('PROTOCOL_SHORT', 'ws');
 define('HOST', 'chat'); // 'localhost/chat'
 define('PORT', 8090);
 define('IP_LISTEN', '0.0.0.0');
-define('SERVER_PATH', '/server.php');
-define('WebForMyself', 'WebForMyself' . SERVER_PATH);
-define('Workerman', 'Workerman' . '/worker.php');
-define('Ratchet', 'Ratchet' . SERVER_PATH);
+define('SERVER_PATH', 'server.php');
+define('WebForMyself', 'WebForMyself/' . SERVER_PATH);
+define('Workerman', 'Workerman/' . SERVER_PATH);
+define('Ratchet', 'Ratchet/' . SERVER_PATH);
 define('SERVER', Workerman);

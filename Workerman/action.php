9b7bdebf7aa80b7fa673d480241f6f401f0bdff6
Nikolay Mishin <mishin.nikolay.d3@mail.ru>
2021-08-12 13:27:19 +0300

    Update action.php

--- Workerman/action.php
+++ Workerman/action.php
@@ -5,15 +5,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use worker\Chat;
 
 $action = $_POST['action'] ?? null;
-$pkey = $_POST['pkey'] ?? null;
-
-if ($action) {
-    switch ($action) {
-		case('start'):
-			Chat::$action();
-			break;
-		case('stop'):
-			Chat::$action($pkey);
-			break;
-	}
-}
+Chat::$action();
